ea64f050c22da20888a1a944c399d536a8b0a7b0
eBrinza <u.andrei678@gmail.com>
2023-09-25 16:38:53 -0400

    Update main.cpp
    Added a binary tree with height 5 for testing inOrder, preOrder, and postOrder.

--- src/main.cpp
+++ src/main.cpp
@@ -12,11 +12,32 @@ using namespace std;
 int main(){
 
 	AVL_Tree test;
-	test.insertNode(3);
-	test.insertNode(2);
-	test.insertNode(5);
+
+	test.insertNode(25);
+	test.insertNode(20);
+	test.insertNode(36);
+	test.insertNode(10);
+	test.insertNode(22);
+	test.insertNode(30);
+	test.insertNode(40);
+	test.insertNode(5);  
+	test.insertNode(12);
+	test.insertNode(28);
+	test.insertNode(38);
+	test.insertNode(48);
+	test.insertNode(1);
+	test.insertNode(8);
+	test.insertNode(15);
+	test.insertNode(45);
+	test.insertNode(50);
 
 	test.inOrder();
+	std::cout << std::endl;
+	test.preOrder();
+	std::cout << std::endl;
+	test.postOrder();
+	std::cout << std::endl;
+	std::cout << "Tree height: " << test.treeHeight() << std::endl;
 
 	return 0;
 }
