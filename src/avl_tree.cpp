e6c9bd0d67f58dd63a9e6a4cb6ed02dc86a1c1ca
eBrinza <u.andrei678@gmail.com>
2023-09-25 14:34:43 -0400

    Updated avl implementations

--- src/avl_tree.cpp
+++ src/avl_tree.cpp
@@ -1,46 +1,42 @@
 #include "avl_tree_node.h"
 
-void AVL_Tree::insertNode(std::string val, int key) {
-	
+AVL_Tree::AVL_Tree() { 
+	this->root = nullptr;
+	std::cout << "Tree created!" << std::endl; 
+}
+AVL_Tree::~AVL_Tree() { std::cout << "Tree destroyed!" << std::endl; }
+
+/*===== Insertion =====*/
+AVL_Node* AVL_Tree::insertNodeHelper(AVL_Node* root, int key) {
 	AVL_Node* new_node = new AVL_Node(key);
-	
-	// Empty Tree, add root.
-	if (this->root == nullptr) {
-		this->root = new_node;
-		this->root->setValue(val);
-		this->root->setBalanceFactor(0);
-		this->tree.push_back(this->root);
-		return;
-	}
-	
-	// All other cases.
-	AVL_Node* dummy = nullptr;
-	AVL_Node* curr = this->root;
-	while (curr != nullptr) {
-		if (curr > new_node) {
-			dummy = curr;
-			curr = curr->left;
-		}
-		else if (curr < new_node) {
-			dummy = curr;
-			curr = curr->right;
-		}
-	}
 
-	new_node->setValue(val);
-	if (dummy > new_node) {
-		dummy->left = new_node;
-		this->tree.push_back(new_node);
-	}
-	else if (dummy < new_node) {
-		dummy->right = new_node;
-		this->tree.push_back(new_node);
-	}
+	// Empty tree, add root.
+	if (root == nullptr)
+		return new_node;
 
+	// Check if it is less than, to go to left subtree, else go to right subtree.
+	if (new_node < root)
+		root->left = insertNodeHelper(root->left, key);
+	else
+		root->right = insertNodeHelper(root->right, key);
+
+	return root;
+}
+void AVL_Tree::insertNode(int key) {
+	this->root = insertNodeHelper(this->root, key);
 }
 
-void AVL_Tree::printTree() {
-	for (unsigned int i = 1; i < this->tree.size(); ++i) {
-		std::cout << "Node, key: " << this->tree.at(i)->getKey() << ", value: " << this->tree.at(i)->getValue() << std::endl;
+/*===== DFS Traversals =====*/
+void AVL_Tree::inOrderHelper(AVL_Node* root) {
+	if (root == nullptr) {
+		std::cout << "";
 	}
+	else {
+		inOrderHelper(root->left);
+		std::cout << root->key << " ";
+		inOrderHelper(root->right);
+	}
+}
+void AVL_Tree::inOrder() {
+	this->inOrderHelper(this->root);
 }
